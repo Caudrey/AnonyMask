496a7a4a3eecd01dc623bead51be92abd8fe711c
Caudrey <103728671+Caudrey@users.noreply.github.com>
2025-06-02 00:27:42 +0700

    try connecting rust + angular

--- ShadowMask/src-tauri/src/main.rs
+++ ShadowMask/src-tauri/src/main.rs
@@ -1,6 +1,44 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  app_lib::run();
+use tauri::Manager;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! This is Rust speaking.", name)
 }
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+// fn main() {
+//     // tauri::Builder::default()
+//     //     .invoke_handler(tauri::generate_handler![run_model])
+//     //     .run(tauri::generate_context!())
+//     //     .expect("error while running tauri application");
+// }
+
+// use std::path::PathBuf;
+// use tauri::command;
+// use tch::{Tensor, CModule};
+
+// #[command]
+// fn run_model(input: Vec<f32>) -> Result<String, String> {
+//     let model_path = PathBuf::from("assets/model/model.pt");
+
+//     let model = CModule::load(&model_path)
+//         .map_err(|e| format!("Failed to load model: {}", e))?;
+
+//     let input_tensor = Tensor::of_slice(&input).view([1, input.len() as i64]);
+//     let output = model
+//         .forward_ts(&[input_tensor])
+//         .map_err(|e| format!("Failed to run model: {}", e))?;
+
+//     let out_vec: Vec<f32> = Vec::<f32>::from(output);
+//     Ok(format!("{:?}", out_vec))
+// }
+
--- ShadowMask/src/app/app.ts
+++ ShadowMask/src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { invoke } from '@tauri-apps/api/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'ShadowMask';
+
+  // result = '';
+
+  // callRust() {
+  //   invoke<string>('greet', { name: 'Angular Developer' })
+  //     .then(response => this.result = response)
+  //     .catch(err => console.error(err));
+  // }
+
+  name = '';
+  message = '';
+
+  async greet() {
+    this.message = await invoke('greet', { name: this.name });
+  }
 }
